332095349ecc7cbef1d0b6b9a7f17d9d940d3369
MarceloGomesFollow <marcelojg@gmail.com>
2025-06-22 17:42:43 -0300

    atualização user.py no models
    atualização do arquivo user.py no models

--- chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
+++ chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
@@ -1,39 +1,17 @@
-from flask import Blueprint, jsonify, request
-from src.models.user import User, db
+from flask_sqlalchemy import SQLAlchemy
 
-user_bp = Blueprint('user', __name__)
+db = SQLAlchemy()
 
-@user_bp.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([user.to_dict() for user in users])
+class User(db.Model):
+    __tablename__ = 'users'
 
-@user_bp.route('/users', methods=['POST'])
-def create_user():
-    
-    data = request.json
-    user = User(username=data['username'], email=data['email'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
 
-@user_bp.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify(user.to_dict())
-
-@user_bp.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.json
-    user.username = data.get('username', user.username)
-    user.email = data.get('email', user.email)
-    db.session.commit()
-    return jsonify(user.to_dict())
-
-@user_bp.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return '', 204
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
