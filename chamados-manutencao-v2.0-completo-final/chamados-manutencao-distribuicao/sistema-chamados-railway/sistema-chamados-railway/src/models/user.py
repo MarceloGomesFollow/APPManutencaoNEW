608abd2543a374332894a71489473e695a3a4c2b
MarceloGomesFollow <marcelojg@gmail.com>
2025-06-22 18:20:14 -0300

    att3
    att3

--- chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
+++ chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    id_perfil = db.Column(db.Integer, db.ForeignKey('tb_perfis.id')) 
 
     def to_dict(self):
         return {
