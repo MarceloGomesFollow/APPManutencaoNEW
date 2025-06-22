4939f9345f3963b738a5ca9944ab5ff6c70a19b6
MarceloGomesFollow <marcelojg@gmail.com>
2025-06-22 18:12:51 -0300

    Update user.py

--- chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
+++ chamados-manutencao-v2.0-completo-final/chamados-manutencao-distribuicao/sistema-chamados-railway/sistema-chamados-railway/src/models/user.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'tb_users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
