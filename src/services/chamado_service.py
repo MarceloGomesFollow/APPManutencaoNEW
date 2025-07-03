0e6e8fb3952652ba293ef6f52d5d0758628d5753
LPaiva0 <163358238+LPaiva0@users.noreply.github.com>
2025-07-02 21:07:48 -0300

    Update chamado_service.py

--- src/services/chamado_service.py
+++ src/services/chamado_service.py
@@ -1,12 +1,39 @@
 # src/services/chamado_service.py
 
+#class ChamadoService:
+    #def __init__(self):
+        # Pode receber db, config, etc
+      #  pass
+
+  #  def criar_chamado(self, titulo, descricao, local_id):
+        # Aqui vai a lógica real (ex: salvar no banco)
+        # No exemplo, só printa os dados
+     #   print(f"Chamado criado: Título={titulo}, Descrição={descricao}, Local={local_id}")
+      #  return {"status": "sucesso", "mensagem": "Chamado criado com sucesso"}
+
+from src.extensions import db
+from src.models import Chamado
+from datetime import datetime
+
 class ChamadoService:
     def __init__(self):
-        # Pode receber db, config, etc
         pass
 
     def criar_chamado(self, titulo, descricao, local_id):
-        # Aqui vai a lógica real (ex: salvar no banco)
-        # No exemplo, só printa os dados
-        print(f"Chamado criado: Título={titulo}, Descrição={descricao}, Local={local_id}")
-        return {"status": "sucesso", "mensagem": "Chamado criado com sucesso"}
+        chamado = Chamado(
+            titulo=titulo,
+            descricao=descricao,
+            local_especifico=local_id,
+            status="Aberto",
+            data_criacao=datetime.utcnow(),
+            protocolo=self.gerar_protocolo()
+        )
+
+        db.session.add(chamado)
+        db.session.commit()
+        return chamado
+
+    def gerar_protocolo(self):
+        
+        from uuid import uuid4
+        return str(uuid4())[:8].upper()
